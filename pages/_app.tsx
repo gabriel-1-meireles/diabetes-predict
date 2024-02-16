dcd834298a71cf2d5efb5bf22cdab7f094ea001b
Gabriel <gabrielmeireles@pm.me>
2024-02-16 18:47:02 +0000

    add `animations.css`

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from "next/app";
 import "../styles/globals.css";
+import "../styles/animations.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return <Component {...pageProps} />;
